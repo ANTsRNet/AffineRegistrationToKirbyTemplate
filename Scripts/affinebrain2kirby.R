9de2e272611bfbad90f6dc6809994d20346538dc
stnava <stnava@gmail.com>
2018-12-17 08:04:10 -0500

    WIP: btr wts

--- Scripts/affinebrain2kirby.R
+++ Scripts/affinebrain2kirby.R
@@ -56,16 +56,17 @@ load_model_weights_hdf5( regressionModel, paste0( rdir, "Data/brainAffinealgResN
 
 newimg = antsImageRead( inputFileName ) %>% iMath("Normalize")
 centerOfMassTemplate <- getCenterOfMass( template )
+centerOfMassTemplate = c( 101.3143, 140.8385, 140.7970 ) # this was used in training
 centerOfMassImage <- getCenterOfMass( newimg )
 
 # do a voting type of thing
 set.seed(1)
-ntx = 10
+ntx = 8
 myarr = array( dim = c( ntx, dim( refH ), 1 ) )
 xfrmList = list()
 txmat = matrix( 0, nrow=ntx, ncol=3 )
 for ( k in 1:ntx ) {
-  if ( k > 1 ) txmat[k,]  = rnorm(3,0,0.5)*antsGetSpacing(template)
+  if ( k > 1 ) txmat[k,]  = rnorm(3,0,1)*antsGetSpacing(template)
   xfrm <- createAntsrTransform( type = "Euler3DTransform",
       center = centerOfMassTemplate,
       translation = centerOfMassImage - centerOfMassTemplate + txmat[k,] )
@@ -76,33 +77,47 @@ for ( k in 1:ntx ) {
   }
 predParams = regressionModel %>% predict( myarr, verbose = 1 )
 pcaReconCoeffsMeans = read.csv( paste0( rdir, "Data/brainAffinealgResNetscl4regressionModelmn.csv" ) )[,1]
-for ( k in 1:1 ) {
+bestk=0
+bestmi=Inf
+for ( k in 1:ntx ) {
   inp = predParams[k,] + pcaReconCoeffsMeans # predicted solution
   if ( k == 1 ) inp = colMeans( predParams ) + pcaReconCoeffsMeans
   affTx = createAntsrTransform( "AffineTransform", dimension = 3 )
-  trupfix = getAntsrTransformFixedParameters( xfrmList[[k]] )
-  setAntsrTransformFixedParameters( affTx, trupfix[1:3])
+  setAntsrTransformFixedParameters( affTx, centerOfMassTemplate )
   setAntsrTransformParameters( affTx, inp )
   trnmat = paste0( outputFileName, "translation.mat" )
   writeAntsrTransform( xfrmList[[k]], trnmat )
   affmat = paste0( outputFileName, "learnedAffine.mat" )
   writeAntsrTransform( invertAntsrTransform( affTx ), affmat )
-
-  affout = paste0( outputFileName, "learnedAffine.nii.gz" )
   tx = c( affmat, trnmat )
   learnedi = antsApplyTransforms( templateH, newimg, tx )
-  antsImageWrite( learnedi, affout )
-  tarimgHi = antsApplyTransforms( template, newimg, tx[2] )
   print( k )
-  print( antsImageMutualInformation( template, tarimgHi*brainSeg) )
-  print( antsImageMutualInformation( template, learnedi*brainSeg) )
+  refmi =  antsImageMutualInformation( template, learnedi ) 
+  if ( refmi < bestmi ) { bestk = k; bestmi = refmi }
 }
+##############################################
+print( paste( 'bestk',bestk,'bestmi',bestmi) )
+k=bestk
+inp = predParams[k,] + pcaReconCoeffsMeans # predicted solution
+if ( k == 1 ) inp = colMeans( predParams ) + pcaReconCoeffsMeans
+affTx = createAntsrTransform( "AffineTransform", dimension = 3 )
+setAntsrTransformFixedParameters( affTx, centerOfMassTemplate )
+setAntsrTransformParameters( affTx, inp )
+trnmat = paste0( outputFileName, "translation.mat" )
+writeAntsrTransform( xfrmList[[k]], trnmat )
+affmat = paste0( outputFileName, "learnedAffine.mat" )
+writeAntsrTransform( invertAntsrTransform( affTx ), affmat )
+affout = paste0( outputFileName, "learnedAffine.nii.gz" )
+tx = c( affmat, trnmat )
+learnedi = antsApplyTransforms( templateH, newimg, tx )
+antsImageWrite( learnedi, affout )
 
 
 fignms = paste0( outputFileName, c("view1.png", "view2.png" ) )
 print( fignms ) 
-plot( learnedi*100*1, template, outname=fignms[1], doCropping=F, nslices=20, ncolumns=5 , alpha=0.5 , axis=3, quality = 0.5 )
-plot( learnedi*100*1, template, outname=fignms[2], doCropping=F, nslices=20, ncolumns=5 , alpha=0.5 , axis=2,quality = 0.5 )
+myq = 0.8
+plot( learnedi*100*1, template, outname=fignms[1], doCropping=T, nslices=20, ncolumns=5 , alpha=0.5 , axis=3, quality = myq )
+plot( learnedi*100*1, template, outname=fignms[2], doCropping=T, nslices=20, ncolumns=5 , alpha=0.5 , axis=2,quality = myq )
 print("done")
 
 # aderkaea
