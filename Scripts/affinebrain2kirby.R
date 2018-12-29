a4da52f6d256acd998e01b9000440aa0ea5b00bf
brian avants <stnava@gmail.com>
2018-12-29 16:56:34 -0500

    ENH: aff multi

--- Scripts/affinebrain2kirby.R
+++ Scripts/affinebrain2kirby.R
@@ -32,6 +32,16 @@ normimg <-function( img, scl=4 ) {
     resampleImage( 4  )
 }
 
+normimg <-function( img ) {
+  resampleImage( img, 4 ) %>%
+    n3BiasFieldCorrection( 2 ) %>%
+    iMath(  'TruncateIntensity', 0.01, 0.98 ) %>%
+    iMath("Normalize" ) %>%
+#    histogramMatchImage(  template ) %>%
+    iMath("Normalize" )
+}
+
+
 numRegressors = 10
 refH = normimg( template )
 input_shape <- c( dim( refH ), refH@components )
@@ -46,14 +56,47 @@ centerOfMassImage <- getCenterOfMass( newimg )
 xfrm <- createAntsrTransform( type = "Euler3DTransform",
   center = centerOfMassTemplate,
   translation = centerOfMassImage - centerOfMassTemplate )
-tarimg = applyAntsrTransformToImage( xfrm, newimg, template )
-nimg = normimg( tarimg, scl=scl )
-nimgarr = array( as.array(nimg), dim = c(1, dim(refH), 1) )
-nimgarr2 = array( as.array(nimg)-as.array(refH), dim = c(1, dim(refH), 1) )
-# predParams = regressionModel %>% predict( list(nimgarr,nimgarr2), verbose = 1 )
-predParams = regressionModel %>% predict( nimgarr, verbose = 1 )
 
-inp =  basis %*% ( mns +  predParams[1,] )
+# do a voting type of thing
+ntx = 50
+myarr = array( dim = c( ntx, dim( refH ), 1 ) )
+xfrmList = list()
+txmat = matrix( 0, nrow=ntx, ncol=3 )
+for ( k in 1:ntx ) {
+  if ( k > 1 ) txmat[k,]  = rnorm(3,0,0.02)*antsGetSpacing(template)
+  xfrm <- createAntsrTransform( type = "Euler3DTransform",
+      center = centerOfMassTemplate,
+      translation = centerOfMassImage - centerOfMassTemplate + txmat[k,] )
+  nimg <- applyAntsrTransformToImage( xfrm, newimg, refH ) %>%
+    normimg()
+  myarr[k,,,,1]= as.array(nimg)
+  xfrmList[[k]] = xfrm
+  }
+predParams = regressionModel %>% predict( myarr, verbose = 1,
+  batch_size = round(ntx/2) )
+bestk=0
+bestmi=Inf
+for ( k in 1:ntx ) {
+  inp =  basis %*% ( mns +  predParams[k,] )
+#  if ( k == 1 ) inp =  basis %*% ( mns +  colMeans( predParams ) )
+  affTx = createAntsrTransform( "AffineTransform", dimension = 3 )
+  setAntsrTransformFixedParameters( affTx, centerOfMassTemplate[1:3] )
+  setAntsrTransformParameters( affTx, inp )
+  affTx = invertAntsrTransform( affTx )
+  trnmat = paste0( outputFileName, "translation.mat" )
+  writeAntsrTransform( xfrm, trnmat )
+  affmat = paste0( outputFileName, "learnedAffine.mat" )
+  writeAntsrTransform( affTx, affmat )
+  tarimgHi = antsApplyTransforms( templateH, newimg, trnmat )
+  tx = c( affmat, trnmat )
+  learnedi2 = antsApplyTransforms( template, newimg, tx  )
+  refmi =  antsImageMutualInformation( template, learnedi2 )
+  print( refmi )
+  if ( refmi < bestmi ) { bestk = k; bestmi = refmi }
+}
+##############################################
+print( paste( 'bestk',bestk,'bestmi',bestmi) )
+inp =  basis %*% ( mns +  predParams[bestk,] )
 affTx = createAntsrTransform( "AffineTransform", dimension = 3 )
 setAntsrTransformFixedParameters( affTx, centerOfMassTemplate[1:3] )
 setAntsrTransformParameters( affTx, inp )
@@ -66,81 +109,18 @@ tarimgHi = antsApplyTransforms( templateH, newimg, trnmat )
 tx = c( affmat, trnmat )
 learnedi2 = antsApplyTransforms( template, newimg, tx  )
 bestmi =  antsImageMutualInformation( template, learnedi2 )
-print( antsImageMutualInformation( template, tarimgHi ) )
+# print( antsImageMutualInformation( template, tarimgHi ) )
 print( antsImageMutualInformation( template, learnedi2 ) )
 affout = paste0( outputFileName, "learnedAffine.nii.gz" )
 antsImageWrite( learnedi2, affout )
 
-inp1 = inp
-fignms = paste0( outputFileName, c("view1.png", "view2.png" ) )
+
+if ( TRUE ) {
+fignms = paste0( outputFileName, c("view1v.png", "view2v.png" ) )
 print( fignms )
 myq = 0.8
 plot( learnedi2*100*1, template, outname=fignms[1], doCropping=T, nslices=20, ncolumns=5 , alpha=0.5 , axis=3, quality = myq )
 plot( learnedi2*100*1, template, outname=fignms[2], doCropping=T, nslices=20, ncolumns=5 , alpha=0.5 , axis=2,quality = myq )
-print("done")
-q("no")
-
-# derka
-
-# do a voting type of thing
-set.seed(1)
-ntx = 22
-myarr = array( dim = c( ntx, dim( refH ), 1 ) )
-xfrmList = list()
-txmat = matrix( 0, nrow=ntx, ncol=3 )
-for ( k in 1:ntx ) {
-  if ( k > 1 ) txmat[k,]  = rnorm(3,0,0.02)*antsGetSpacing(template)
-  xfrm <- createAntsrTransform( type = "Euler3DTransform",
-      center = centerOfMassTemplate,
-      translation = centerOfMassImage - centerOfMassTemplate + txmat[k,] )
-  nimg <- applyAntsrTransformToImage( xfrm, newimg, refH ) %>% 
-    iMath("Normalize")
-  myarr[k,,,,1]= as.array(nimg)
-  xfrmList[[k]] = xfrm
-  }
-predParams = regressionModel %>% predict( myarr, verbose = 1 )
-bestk=0
-for ( k in 1:ntx ) {
-#  inp = predParams[k,] + mns # predicted solution
-  inp =  basis %*% ( mns +  predParams[k,] )
-  if ( k == 1 ) inp = basis %*% ( colMeans( predParams ) + mns )
-  affTx = createAntsrTransform( "AffineTransform", dimension = 3 )
-  setAntsrTransformFixedParameters( affTx, centerOfMassTemplate )
-  setAntsrTransformParameters( affTx, inp )
-  trnmat = paste0( outputFileName, "translation.mat" )
-  writeAntsrTransform( xfrmList[[k]], trnmat )
-  affmat = paste0( outputFileName, "learnedAffine.mat" )
-#  writeAntsrTransform( invertAntsrTransform( affTx ), affmat )
-  writeAntsrTransform( affTx, affmat )
-  tx = c( affmat, trnmat )
-  learnedi = antsApplyTransforms( templateH, newimg, tx )
-  refmi =  antsImageMutualInformation( template, learnedi ) 
-  if ( refmi < bestmi ) { bestk = k; bestmi = refmi }
-}
-##############################################
-print( paste( 'bestk',bestk,'bestmi',bestmi) )
-k=bestk
-inp =  basis %*% ( mns +  predParams[k,] )
-if ( k == 1 ) inp = basis %*% ( colMeans( predParams ) + mns )
-affTx = createAntsrTransform( "AffineTransform", dimension = 3 )
-setAntsrTransformFixedParameters( affTx, centerOfMassTemplate )
-setAntsrTransformParameters( affTx, inp )
-trnmat = paste0( outputFileName, "translation.mat" )
-writeAntsrTransform( xfrmList[[k]], trnmat )
-affmat = paste0( outputFileName, "learnedAffine.mat" )
-# writeAntsrTransform( invertAntsrTransform( affTx ), affmat )
-writeAntsrTransform( affTx, affmat )
-affout = paste0( outputFileName, "learnedAffine.nii.gz" )
-tx = c( affmat, trnmat )
-learnedi = antsApplyTransforms( templateH, newimg, tx )
-antsImageWrite( learnedi, affout )
-
-if ( TRUE ) {
-fignms = paste0( outputFileName, c("view1v.png", "view2v.png" ) )
-print( fignms ) 
-myq = 0.8
-plot( learnedi*100*1, template, outname=fignms[1], doCropping=T, nslices=20, ncolumns=5 , alpha=0.5 , axis=3, quality = myq )
-plot( learnedi*100*1, template, outname=fignms[2], doCropping=T, nslices=20, ncolumns=5 , alpha=0.5 , axis=2,quality = myq )
 print("done")
 }
 q("no")
@@ -160,4 +140,3 @@ plot(  refH, doCropping=F, nslices=20, ncolumns=5 , alpha=0.5 , axis=2 )
 dreg = antsRegistration( templateH, tarimg, "SyNOnly", initialTransform =  invertAntsrTransform( affTx ) )
 plot(  dreg$warpedmovout*100*1, doCropping=F, nslices=20, ncolumns=5 , alpha=0.5 , axis=2 )
 plot( template, doCropping=F, nslices=20, ncolumns=5 , alpha=0.5 , axis=2 )
-
